9851e437a123c9e8eeb6c12d82805299e04367a9
Tsarina <tsarina.islam@gmail.com>
2019-09-11 14:09:15 -0400

    original react template, some .css and someimports

---
+++ clickygame/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './cards.json';
+
+class App extends Component {
+  // Basically all the components need to get within this render
+
+  render () {
+    return ({})
+  }
+
+}
+
+export default App;
---
+++ clickygame/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1>Pokemon Pic Click</h1>
+      <p>
+        Click on a pic, but don't click on any pic more than once!
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;
